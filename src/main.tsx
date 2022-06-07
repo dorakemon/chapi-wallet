d6a317084eba84327e95a786a248eaf194daea1d
dorakemon <51844896+dorakemon@users.noreply.github.com>
2022-06-07 12:00:00 +0900

    add: i18n support

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 import "./index.css";
+import "./i18n/config";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
